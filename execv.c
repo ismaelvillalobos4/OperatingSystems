261bc7873f81bf7a097bf7dbd647db92bf984a1e
Ismael Villalobos <47580906+ismaelvillalobos4@users.noreply.github.com>
2021-09-15 14:26:06 -0600

    Create execv.c

---
+++ execv.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>	/* needed to define exit() */
+#include <unistd.h>	/* needed to define getpid() */
+#include <stdio.h>	/* needed for printf() */
+
+int
+main(int argc, char **argv) {
+	char *args[] = {"ls", "-aF", "/", 0};	/* each element represents a command line argument */
+
+	printf("About to run ls\n");
+	execvp("ls", args);
+	perror("execvp");	/* if we get here, execvp failed */
+	exit(1);
+}
